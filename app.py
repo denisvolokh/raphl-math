4f49f4197661fcd61bf442b7449f1ffc8639dfd6
Denis Volokh <barmalei4ik@gmail.com>
2013-05-04 16:17:45 +0800

    moved to pymongo

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask, request, redirect, render_template, send_from_directory
 import csv
-from urlparse import urlparse 
-from mongoengine import connect, Document, StringField, DateTimeField, FloatField, DictField,BooleanField, IntField
+from urlparse import urlparse
+from pymongo import MongoClient
 from datetime import datetime
 from flask import jsonify
-import json
+from bson.json_util import dumps
 
 #----------------------------------------
 # initialization
@@ -19,59 +19,64 @@ app.config.update(
 
 ALLOWED_EXTENSIONS = set(['csv',])
 
-database = urlparse(os.environ.get("MONGOHQ_URL", "mongodb://localhost/raphl-math"))
+database = urlparse(os.environ.get("MONGOHQ_URL"))
+print "[+] ", database
 
-connect(database.path[1:],
-		host=database.hostname,
-		port=database.port,
-		username=database.username,
-		password=database.password)
+client = MongoClient(database)
+db = client['raphl-math']
+print db.collection_names()
 
-class DataSet(Document):
-	name = StringField()
-	created = DateTimeField()
+# connect(database.path[1:],
+# 		host=database.hostname,
+# 		port=database.port,
+# 		username=database.username,
+# 		password=database.password)
 
-	def to_dict(self):
-		return mongo_to_dict_helper(self)
+# class DataSet(Document):
+# 	name = StringField()
+# 	created = DateTimeField()
 
-class Record(Document):
-	dataset_id = StringField()
-	date = StringField()
-	created = DateTimeField()
-	open = FloatField()
-	high = FloatField()
-	low = FloatField()
-	last_price=FloatField()
-	action=StringField()
-	vol=FloatField()
-	stop1=FloatField()
-	target1=FloatField()
-	target2=FloatField()
+# 	def to_dict(self):
+# 		return mongo_to_dict_helper(self)
 
-	highlight=BooleanField(default=False)
+# class Record(Document):
+# 	dataset_id = StringField()
+# 	date = StringField()
+# 	created = DateTimeField()
+# 	open = FloatField()
+# 	high = FloatField()
+# 	low = FloatField()
+# 	last_price=FloatField()
+# 	action=StringField()
+# 	vol=FloatField()
+# 	stop1=FloatField()
+# 	target1=FloatField()
+# 	target2=FloatField()
 
-	def to_dict(self):
-		return mongo_to_dict_helper(self)
+# 	highlight=BooleanField(default=False)
 
-def mongo_to_dict_helper(obj):
-	return_data = []
-	for field_name in obj._fields:
-		if field_name in ("id",):
-			continue
+# 	def to_dict(self):
+# 		return mongo_to_dict_helper(self)
 
-		data = obj._data[field_name]
+# def mongo_to_dict_helper(obj):
+# 	return_data = []
+# 	for field_name in obj._fields:
+# 		if field_name in ("id",):
+# 			continue
 
-		if isinstance(obj._fields[field_name], StringField):
-			return_data.append((field_name, str(data)))
-		elif isinstance(obj._fields[field_name], FloatField):
-			return_data.append((field_name, float(data)))
-		elif isinstance(obj._fields[field_name], BooleanField):
-			return_data.append((field_name, bool(data)))
-		# elif isinstance(obj._fields[field_name], ListField):
-		# 	return_data.append((field_name, data))
-		# else:
-            # You can define your logic for returning elements
-	return dict(return_data)
+# 		data = obj._data[field_name]
+
+# 		if isinstance(obj._fields[field_name], StringField):
+# 			return_data.append((field_name, str(data)))
+# 		elif isinstance(obj._fields[field_name], FloatField):
+# 			return_data.append((field_name, float(data)))
+# 		elif isinstance(obj._fields[field_name], BooleanField):
+# 			return_data.append((field_name, bool(data)))
+# 		# elif isinstance(obj._fields[field_name], ListField):
+# 		# 	return_data.append((field_name, data))
+# 		# else:
+#             # You can define your logic for returning elements
+# 	return dict(return_data)
 
 #----------------------------------------
 # controllers
@@ -80,23 +85,23 @@ def mongo_to_dict_helper(obj):
 @app.route('/listrecords', methods=['GET', 'POST'])
 def list_records():
 	id = request.args["dataset_id"]
-	_marked = []
-
-	records = Record.objects(dataset_id=id)
+	records = db["records"].find({"file_id" : str(id)})
+	
 	marked = mark_records_buy_action(records, "SELL")
 	marked = mark_records_buy_action(marked, "BUY")
 
-	for rec in marked:		
-		_marked.append(mongo_to_dict_helper(rec))
-	
-	return json.dumps(_marked)
+	return dumps(list(marked))
 
-@app.route('/listdataset', methods=['GET', 'POST'])
-def list_data_set():
-	datasets = DataSet.objects.all()
-	print "[+] ", datasets.count()
-	print datasets.to_json()
-	return datasets.to_json()
+	# for rec in marked:		
+		# _marked.append(mongo_to_dict_helper(rec))
+	
+	# return json.dumps(_marked)
+
+@app.route('/listfiles', methods=['GET', 'POST'])
+def list_files():
+	files = db["files"].find()
+	print "[+] ", files.count()
+	return dumps(list(files))
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -104,29 +109,37 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        given_name = request.form["given_name"]
-        if file and allowed_file(file.filename):
-        	dataset = DataSet(name=file.filename)
-        	dataset.save()
+	print "[+] Uploading file..."
+	if request.method == 'POST':
+		print "[+] Files: ", request.files
+		file = request.files['file']
+		print "[+] Files: ", file
+    	given_name = request.form["name"]
+    	if given_name == None:
+    		given_name = file.filename
+    	print "[+] Custom name: ", given_name
+    	if file and allowed_file(file.filename):
+        	_file = {
+        		"name": given_name
+        	}
+        	file_id = db["files"].insert(_file)
         	for row in csv.DictReader(file.stream):
     				if row["Date/Time"] != "":
-    					record = Record(dataset_id=str(dataset.id),
-    									date=row["Date/Time"], 
-    									open=row["OPEN"], 
-    									high=row["HIGH"], 
-    									low=row["LOW"],
-    									last_price=row["LAST_PRICE"],
-    									action=row["Action"],
-    									vol=row["Vol"],
-    									stop1=row["STOP1"],
-    									target1=row["Target 1"],
-    									target2=row["Target 2"],
-    									created=datetime.utcnow())	
-    					record.save()
-	redirect("/")    					
-    return ""
+    					record = {
+    						"file_id": str(file_id),
+    						"date" : row["Date/Time"],
+    						"open" : row["OPEN"],
+    						"high" : row["HIGH"],
+    						"low" : row["LOW"],
+    						"last_price" : row["LAST_PRICE"],
+    						"action" : row["Action"],
+    						"vol" : row["Vol"],
+    						"stop1" : row["STOP1"],
+    						"target1" : row["Target 1"],
+    						"target2" : row["Target 2"]
+    					}
+    					db["records"].insert(record)
+	return ""
 
 @app.route("/")
 def index():
@@ -144,12 +157,12 @@ def mark_records_buy_action(collection, action="SELL"):
 		if idx != 0:
 			prev = _coll[idx-1]
 			if not skip_subsequent_flag:
-				if item.action == action and prev.action == action:
-					prev.highlight = True
-					item.highlight = True
+				if item["action"] == action and prev["action"] == action:
+					prev["highlight"] = True
+					item["highlight"] = True
 					skip_subsequent_flag = True
 
-			if item.action == "" or item.action == opp_action: 
+			if item["action"] == "" or item["action"] == opp_action: 
 				skip_subsequent_flag = False		
 
 		_coll.append(item)		
--- static/js/controllers/home-controller.js
+++ static/js/controllers/home-controller.js
@@ -1,8 +1,38 @@
 function HomeController($http, $scope, $log) {
 
 
-	$http.get("/listdataset")
+	$http.get("/listfiles")
 		.success(function(data) {
 			$scope.datasets = data;
 		})
+
+	$scope.readyToUpload = true;
+	var uploadFile;
+
+	$scope.setFile = function(element) {
+		uploadFile = element.files[0];
+		if (uploadFile) {
+			$scope.readyToUpload = true;
+		}
+	};	
+		
+	$scope.submitFile = function() {
+		var formData = new FormData();
+		if (angular.isDefined($scope.custom_name))
+			formData.append("name", $scope.custom_name);
+		if (angular.isDefined(uploadFile))
+			formData.append("file", uploadFile);
+
+		var xhr = new XMLHttpRequest;
+		xhr.onreadystatechange = function(event) {
+			$http.get("/listfiles")
+				.success(function(data) {
+				$scope.datasets = data;
+			})
+				$scope.custom_name = "";
+		};
+
+		xhr.open('POST', '/upload', true);	
+	    xhr.send(formData);
+	}	
 }
